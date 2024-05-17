77a99195f9b5d6fbd51f7935d5c5f957f63eb064
xvisox <hubertmirek@gmail.com>
2024-05-17 19:30:27 +0200

    Changes by alek

--- src/main/java/pl/mimuw/allezon/domain/Product.java
+++ src/main/java/pl/mimuw/allezon/domain/Product.java
@@ -10,46 +10,11 @@ import lombok.extern.jackson.Jacksonized;
 @Builder
 public class Product {
     @JsonProperty("product_id")
-    private int productId;
+    int productId;
     @JsonProperty("brand_id")
-    private String brandId;
+    String brandId;
     @JsonProperty("category_id")
-    private String categoryId;
+    String categoryId;
     @JsonProperty("price")
-    private int price;
-
-    public Product() {
-    }
-
-    public int getProductId() {
-        return productId;
-    }
-
-    public void setProductId(int productId) {
-        this.productId = productId;
-    }
-
-    public String getBrandId() {
-        return brandId;
-    }
-
-    public void setBrandId(String brandId) {
-        this.brandId = brandId;
-    }
-
-    public String getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(String categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
+    int price;
 }
