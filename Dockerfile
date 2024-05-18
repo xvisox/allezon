ec53537fa2456651ae523c05423f4784fcd81795
xvisox <hubertmirek@gmail.com>
2024-05-18 20:39:05 +0200

    run docker script

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,4 @@
-FROM openjdk:17.0.2-slim-buster
-
-RUN groupadd -g 10240 worker && \
-    useradd -r -u 10240 -g worker worker
-
-USER worker:worker
-
-ARG JAR_FILE
-
-ADD ${JAR_FILE} /app/app.jar
-
-ENTRYPOINT java \
-    -Xmx2g \
-    -jar /app/app.jar
+FROM openjdk:17
+WORKDIR /app
+COPY target/allezon.jar /app/allezon.jar
+ENTRYPOINT ["java", "-jar", "allezon.jar"]
--- scripts/run_docker.sh
+++ scripts/run_docker.sh
@@ -5,7 +5,8 @@ USER="st122"
 HOST="st122vm101.rtb-lab.pl"
 REMOTE_DIR="/home/${USER}"
 LOCAL_DIR="../docker"
-ANSIBLE_PLAYBOOK="docker-playbook.yaml"
+DOCKER_PLAYBOOK="docker-playbook.yaml"
+SWARM_PLAYBOOK="swarm-playbook.yaml"
 INVENTORY="hosts"
 EXTRA_VARS="ansible_user=${USER} ansible_password=${PASSWORD} ansible_ssh_extra_args='-o StrictHostKeyChecking=no'"
 
@@ -13,5 +14,6 @@ sshpass -p "${PASSWORD}" scp -r "${LOCAL_DIR}" ${USER}@${HOST}:${REMOTE_DIR}
 
 sshpass -p "${PASSWORD}" ssh ${USER}@${HOST} << EOF
 cd docker
-ansible-playbook --extra-vars "${EXTRA_VARS}" -i ${INVENTORY} ${ANSIBLE_PLAYBOOK}
+ansible-playbook --extra-vars "${EXTRA_VARS}" -i ${INVENTORY} ${DOCKER_PLAYBOOK}
+ansible-playbook --extra-vars "${EXTRA_VARS}" -i ${INVENTORY} ${SWARM_PLAYBOOK}
 EOF
