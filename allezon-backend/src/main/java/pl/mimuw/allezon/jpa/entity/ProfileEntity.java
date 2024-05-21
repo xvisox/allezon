215a18943308f353bc7a923abc8b3b02e3ef04cb
xvisox <hubertmirek@gmail.com>
2024-05-21 20:43:43 +0200

    add profiles compression

--- allezon-backend/src/main/java/pl/mimuw/allezon/jpa/entity/ProfileEntity.java
+++ allezon-backend/src/main/java/pl/mimuw/allezon/jpa/entity/ProfileEntity.java
@@ -1,29 +1,70 @@
 package pl.mimuw.allezon.jpa.entity;
 
-import lombok.AllArgsConstructor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Getter;
-import lombok.ToString;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.aerospike.mapping.Document;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Version;
+import org.xerial.snappy.Snappy;
 import pl.mimuw.allezon.Constants;
 import pl.mimuw.allezon.domain.UserTag;
 
+import java.io.IOException;
 import java.util.List;
 
-@ToString
-@Getter
-@AllArgsConstructor
+@Setter
+@NoArgsConstructor
 @Document(collection = Constants.PROFILES_COLLECTION)
 public class ProfileEntity {
+    private static final ObjectMapper objectMapper = new ObjectMapper();
 
     @Id
     private String cookie;
 
+    @Getter
     @Version
     private int generation;
 
-    private List<UserTag> views;
+    private byte[] compressedViews;
 
-    private List<UserTag> buys;
+    private byte[] compressedBuys;
+
+    public ProfileEntity(final String cookie, final int generation,
+                         final List<UserTag> views, final List<UserTag> buys) {
+        this.cookie = cookie;
+        this.generation = generation;
+        this.compressedViews = compress(views);
+        this.compressedBuys = compress(buys);
+    }
+
+    public List<UserTag> getViews() {
+        return decompress(compressedViews);
+    }
+
+    public List<UserTag> getBuys() {
+        return decompress(compressedBuys);
+    }
+
+    private byte[] compress(final List<UserTag> userTags) {
+        try {
+            String json = objectMapper.writeValueAsString(userTags);
+            return Snappy.compress(json);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to compress data", e);
+        }
+    }
+
+    private List<UserTag> decompress(final byte[] compressedData) {
+        try {
+            if (compressedData == null) {
+                return null;
+            }
+            final String json = Snappy.uncompressString(compressedData);
+            return objectMapper.readValue(json, objectMapper.getTypeFactory().constructCollectionType(List.class, UserTag.class));
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to decompress data", e);
+        }
+    }
 }
